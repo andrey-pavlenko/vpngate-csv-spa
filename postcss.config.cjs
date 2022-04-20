c0a837081887986d517c149ce725093438fe2ad0
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2022-04-20 15:28:52 +0300

    vite init

---
+++ postcss.config.cjs
@@ -0,0 +1,16 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {}
+//   }
+// };
+
+module.exports = {
+  plugins: {
+    'postcss-simple-vars': {},
+    'postcss-import': {},
+    'postcss-nested': {},
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import Application from './Application.svelte';
+
+const app = new Application({
+  target: document.getElementById('app')
+});
+
+export default app;
